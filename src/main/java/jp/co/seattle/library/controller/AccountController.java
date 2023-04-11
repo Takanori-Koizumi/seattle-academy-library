dae48871a4169fb0900a5ed7b3e57dda2066db97
sera <sera@seranoair.lan>
2023-04-11 11:54:52 +0900

    task1-1次コミット修正

--- src/main/java/jp/co/seattle/library/controller/AccountController.java
+++ src/main/java/jp/co/seattle/library/controller/AccountController.java
@@ -40,7 +40,6 @@ public class AccountController {
 	 * @return ホーム画面に遷移
 	 */
 
-	
 	@Transactional
 	@RequestMapping(value = "/createAccount", method = RequestMethod.POST)
 	public String createAccount(Locale locale, @RequestParam("email") String email,
@@ -50,22 +49,22 @@ public class AccountController {
 		logger.info("Welcome createAccount! The client locale is {}.", locale);
 
 		// バリデーションチェック、パスワード一致チェック（タスク１）
-       if(password.length()>= 8 && password.matches("[0-9a-zA-Z]+")){
-    	   if(password.equals(passwordForCheck)) {
-    		   // パラメータで受け取ったアカウント情報をDtoに格納する。
-    		   UserInfo userInfo = new UserInfo();
-    		   userInfo.setEmail(email);
-    		   userInfo.setPassword(password);
-    		   usersService.registUser(userInfo);
-    		   return "redirect:/login";
-    	  } else {
-    		  model.addAttribute("errorMessage", "確認用パスワードと一致しません。");
-    		  return "createAccount";
-    	  }
-    	  } else { 
-    		  model.addAttribute("errorMessage", "半角英数字８文字で以上で入力してください。");
-    		  return "createAccount";
-    	  }	
+		if (password.length() >= 8 && password.matches("[0-9a-zA-Z]+")) {
+			if (password.equals(passwordForCheck)) {
+				// パラメータで受け取ったアカウント情報をDtoに格納する。
+				UserInfo userInfo = new UserInfo();
+				userInfo.setEmail(email);
+				userInfo.setPassword(password);
+				usersService.registUser(userInfo);
+				return "redirect:/login";
+			} else {
+				model.addAttribute("errorMessage", "パスワードが一致しません。");
+				return "createAccount";
+			}
+		} else {
+			model.addAttribute("errorMessage", "パスワードは８文字以上かつ半角英数字に設定してください。");
+			return "createAccount";
+		}
 	}
 
 }
