adeb517505b2952e0ad54bc9833672ccd083cb14
sato_minami <sato_minami@192.168.130.172>
2023-04-11 12:41:26 +0900

    task2minami

--- src/main/java/jp/co/seattle/library/controller/LoginController.java
+++ src/main/java/jp/co/seattle/library/controller/LoginController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import jp.co.seattle.library.dto.UserInfo;
 import jp.co.seattle.library.service.UsersService;
 
+
+
 /**
  * ログインコントローラー
  */
@@ -43,7 +45,12 @@ public class LoginController {
 
 		// ユーザーが存在すればログイン、存在しなければエラー(タスク２)
 
+		if(selectedUserInfo != null) {
+			return "home";
+		}else {
+			model.addAttribute("errorMessage", "アカウントが存在しません。");
+			return "redirect:/login";
+		}
 		
-		return "redirect:/home";
 	}
 }
