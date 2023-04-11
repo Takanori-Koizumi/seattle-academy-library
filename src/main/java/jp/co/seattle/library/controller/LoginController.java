6943a6136c27d9828880cad521a62f7a97acea73
sasakawajun <sasakawajun@192.168.130.248>
2023-04-11 12:25:10 +0900

    task2一次コミット

--- src/main/java/jp/co/seattle/library/controller/LoginController.java
+++ src/main/java/jp/co/seattle/library/controller/LoginController.java
@@ -1,5 +1,7 @@
 package jp.co.seattle.library.controller;
 
+import java.util.Objects;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,11 +41,16 @@ public class LoginController {
 	public String login(@RequestParam("email") String email, @RequestParam("password") String password, Model model) {
 
 		// メアドとパスワードに一致するユーザー取得
-		UserInfo selectedUserInfo = usersService.selectUserInfo(email, password);
+		UserInfo selectedUserInfo = usersService.selectUserInfo(email, password);//情報はとっている
 
 		// ユーザーが存在すればログイン、存在しなければエラー(タスク２)
-
+		if(Objects.isNull(selectedUserInfo)) {//
+			model.addAttribute("errorMessage", "存在しません");
+			return "login";
+		}else {
+			return "redirect:/home";
+		}
 		
-		return "redirect:/home";
+		
 	}
 }
