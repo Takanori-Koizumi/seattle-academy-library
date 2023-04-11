aa644af5830f9e79ff36c91574883dfe5f82d0aa
shigesaka <shigesaka@192.168.130.247>
2023-04-11 19:14:35 +0900

    task2 完了

--- src/main/java/jp/co/seattle/library/controller/LoginController.java
+++ src/main/java/jp/co/seattle/library/controller/LoginController.java
@@ -1,11 +1,12 @@
 package jp.co.seattle.library.controller;
 
+import java.util.Objects;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,7 +26,7 @@ public class LoginController {
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String first(Model model) {
-		return "redirect:/login"; // jspファイル名
+		return "login"; // jspファイル名
 	}
 
 	/**
@@ -43,7 +44,7 @@ public class LoginController {
 		UserInfo selectedUserInfo = usersService.selectUserInfo(email, password);
 
 		// ユーザーが存在すればログイン、存在しなければエラー(タスク２)
-		if (!(StringUtils.isEmpty(selectedUserInfo))) {
+		if (Objects.nonNull(selectedUserInfo)) {
 			return "redirect:/home";
 		} else {
 			model.addAttribute("errorMessage", "メールアドレスとパスワードが一致しません。");
