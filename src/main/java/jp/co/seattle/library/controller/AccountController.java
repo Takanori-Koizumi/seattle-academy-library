5af55c171acf7bf44db4e781d3e028fed88183d2
sumidaakari <sumidaakari@localhost>
2023-04-11 17:17:23 +0900

    task1_一次コミット

--- src/main/java/jp/co/seattle/library/controller/AccountController.java
+++ src/main/java/jp/co/seattle/library/controller/AccountController.java
@@ -48,28 +48,24 @@ public class AccountController {
 		logger.info("Welcome createAccount! The client locale is {}.", locale);
 
 		// バリデーションチェック、パスワード一致チェック（タスク１）
-		if (password.length()>=8 && password.matches("[0-9a-zA-Z;]")) {
-			
+		if (password.matches("^[A-Za-z0-9]+$") && password.length() >= 8) {
+			if (password.equals(passwordForCheck)) {
+				UserInfo userInfo = new UserInfo();
+				userInfo.setEmail(email);
+				userInfo.setPassword(password);
+				usersService.registUser(userInfo);
+				return "redirect:/login";
+			} else {
+
+				model.addAttribute("errorMessage", "パスワードが一致しません。");
+				return "createAccount";
+			}
+
 		} else {
 			model.addAttribute("errorMessage", "パスワードは8文字以上かつ半角英数字に設定してください。");
 			return "createAccount";
 		}
-		
-        if (password.equals(passwordForCheck)) {
-        	
-        } else {
-        	model.addAttribute("errorMessage", "パスワードが一致しません。");
-        	return "createAccount";
-        }
-        		
-      
-		
-		// パラメータで受け取ったアカウント情報をDtoに格納する。
-		UserInfo userInfo = new UserInfo();
-		userInfo.setEmail(email);
-		userInfo.setPassword(password);
-		usersService.registUser(userInfo);
-		return "redirect:/login";
+
 	}
 
 }
