377bae48c0bced652048436ce0901cbd101edb9a
misuzuota <misuzuota@localhost>
2023-04-12 12:09:44 +0900

    task2

--- src/main/java/jp/co/seattle/library/controller/LoginController.java
+++ src/main/java/jp/co/seattle/library/controller/LoginController.java
@@ -45,7 +45,7 @@ public class LoginController {
 		if (selectedUserInfo != null) {
 			return "redirect:/home";
 		} else {
-			model.addAttribute("errorMessage", "");
+			model.addAttribute("errorMessage", "パスワードは8文字以上かつ半角英数字に設定してください。");
 			return "createAccount";
 
 		}
