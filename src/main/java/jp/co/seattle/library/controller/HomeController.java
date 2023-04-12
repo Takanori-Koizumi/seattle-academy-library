59bd86b796301c7bb9bb1ad9c150502d15e4e4cb
misuzuota <misuzuota@localhost>
2023-04-12 19:10:54 +0900

    task3

--- src/main/java/jp/co/seattle/library/controller/HomeController.java
+++ src/main/java/jp/co/seattle/library/controller/HomeController.java
@@ -29,7 +29,8 @@ public class HomeController {
 	@RequestMapping(value = "/home", method = RequestMethod.GET)
 	public String transitionHome(Model model) {
 		//書籍の一覧情報を取得（タスク３）
-
+		String sql = "select　bookinfo"
+				
 		return "home";
 	}
 
