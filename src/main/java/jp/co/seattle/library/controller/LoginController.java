0d9b45fb9b613951d8d829d3001b84a42c7f86ef
sasakawajun <sasakawajun@localhost>
2023-04-14 09:41:13 +0900

    task2三次コミット

--- src/main/java/jp/co/seattle/library/controller/LoginController.java
+++ src/main/java/jp/co/seattle/library/controller/LoginController.java
@@ -5,7 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.StringUtils;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -43,12 +43,12 @@ public class LoginController {
 		UserInfo selectedUserInfo = usersService.selectUserInfo(email, password);//情報はとっている
 
 		// ユーザーが存在すればログイン、存在しなければエラー(タスク２)
-		if(!(StringUtils.isEmpty(selectedUserInfo))){
- 			return "redirect:/home";
- 		} else {
- 			model.addAttribute("errorMessage", "メールアドレスとパスワードが一致しません");
- 			return "login";
- 		}
+		if (!(ObjectUtils.isEmpty(selectedUserInfo))) {
+			return "redirect:/home";
+		} else {
+			model.addAttribute("errorMessage", "メールアドレスとパスワードが一致しません");
+			return "login";
+		}
 
 	}
 }
